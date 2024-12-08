5e1723b77fa514b105fa05bda7910c4fd52aa125
HOT SUN <yanx@hosmail.cn>
2024-12-09 01:46:36 +0800

    。。。

--- test/process_manager_test.go
+++ test/process_manager_test.go
@@ -7,12 +7,49 @@ import (
 	"testing"
 )
 
-func TestManagerProcessStatus(t *testing.T) {
+func GetRuntimeConfig() manager.RuntimeConfig {
 	runtime := manager.RuntimeConfig{}
+	return runtime
+}
+
+func TestManagerProcessStart(t *testing.T) {
 	m := manager.GetDefault()
-	health, err := m.Status(context.Background(), runtime, logger.Log())
+	pid, err := m.Start(context.Background(), GetRuntimeConfig(), logger.Log())
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(pid)
+}
+
+func TestManagerProcessRestart(t *testing.T) {
+	m := manager.GetDefault()
+	pid, err := m.Restart(context.Background(), GetRuntimeConfig(), logger.Log())
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(pid)
+}
+
+func TestManagerProcessStop(t *testing.T) {
+	m := manager.GetDefault()
+	err := m.Stop(context.Background(), GetRuntimeConfig(), logger.Log())
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("\r\n-------------   END")
+}
+
+func TestManagerProcessStatus(t *testing.T) {
+	m := manager.GetDefault()
+	health, err := m.Status(context.Background(), GetRuntimeConfig(), logger.Log())
 	if err != nil {
 		t.Fatal(err)
 	}
 	t.Log(health)
+}
+
+func TestManagerProcessClear(t *testing.T) {
+	m := manager.GetDefault()
+	m.Clear()
+	t.Log("\r\n-------------   END")
 }
