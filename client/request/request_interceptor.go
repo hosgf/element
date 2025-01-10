5efb2bdcf9479581f70d9b82a68f967478fbb3f1
hotsun <874488108@qq.com>
2025-01-10 20:44:45 +0800

    ....

--- client/request/request_interceptor.go
+++ client/request/request_interceptor.go
@@ -1,13 +1,15 @@
 package request
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
 )
 
 func RequestInterceptor() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.GetHeader("")
 		ctx := c.Request.Context()
+		c.GetAppCode("")
+		ctx = context.WithValue(ctx, HeaderKey, headers)
 		c.Request = c.Request.WithContext(ctx)
 		c.Next()
 	}
