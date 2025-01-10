27f7863ff1799be3e1177c0e4fadd8358ff7abbc
HOT SUN <yanx@hosmail.cn>
2025-01-11 00:09:34 +0800

    。。。

--- client/ugin/request_interceptor.go
+++ client/ugin/request_interceptor.go
@@ -1,15 +1,16 @@
-package request
+package ugin
 
 import (
 	"context"
 
 	"github.com/gin-gonic/gin"
+	"github.com/hosgf/element/client/request"
 )
 
 func RequestInterceptor() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		ctx := c.Request.Context()
-		for _, header := range GetHeaders() {
+		for _, header := range request.GetHeaders() {
 			ctx = setHandler(ctx, c, header)
 		}
 		c.Request = c.Request.WithContext(ctx)
@@ -17,8 +18,8 @@ func RequestInterceptor() gin.HandlerFunc {
 	}
 }
 
-func setHandler(ctx context.Context, c *gin.Context, header Header) context.Context {
-	if value := GetHeader(c, header); len(value) > 0 {
+func setHandler(ctx context.Context, c *gin.Context, header request.Header) context.Context {
+	if value := request.GetHeader(c, header); len(value) > 0 {
 		ctx = context.WithValue(ctx, header, value)
 		c.Set(header.String(), value)
 	}
